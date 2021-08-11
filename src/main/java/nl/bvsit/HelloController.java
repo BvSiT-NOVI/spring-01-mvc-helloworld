a74e9c635330ed98b28677af6223f94476e0ec1b
BvSiT <34773187+BvSiT@users.noreply.github.com>
2021-08-11 15:44:58 +0200

    First commit

---
+++ src/main/java/nl/bvsit/HelloController.java
@@ -0,0 +1,22 @@
+package nl.bvsit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String currentTime = "Time : "+formatter.format(new Date());
+        String message = "Spring MVC basic example. Refresh your browser to show the current time.";
+        model.addAttribute("message", message);
+        model.addAttribute("time",currentTime);
+        return "hello";
+    }
+}
