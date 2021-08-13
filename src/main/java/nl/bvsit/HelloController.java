0621d6d5ce0c390dd1207c96b85de09ba322eb22
BvSiT <34773187+BvSiT@users.noreply.github.com>
2021-08-13 19:05:16 +0200

    commit

--- src/main/java/nl/bvsit/HelloController.java
+++ src/main/java/nl/bvsit/HelloController.java
@@ -1,5 +1,7 @@
 package nl.bvsit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
